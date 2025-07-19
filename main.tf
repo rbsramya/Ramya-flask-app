78978c1160050e994b1ba7089d1be4ca51a08b9f
rbsramya <rbs.ramya@gmail.com>
2025-07-19 20:30:37 +0800

    added s3 bucket

--- main.tf
+++ main.tf
@@ -12,6 +12,15 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "Ramyacoaching16.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
